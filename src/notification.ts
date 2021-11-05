363ed39cf1a5dc4fe5ddd9cdd30e4e750f534d83
NachGrandi <nachgrandi@gmail.com>
2021-11-05 18:26:20 -0300

    asd

--- src/notification.ts
+++ src/notification.ts
@@ -1,4 +1,10 @@
 import NotificationService from './core/service/notifications';
+import app from './app';
 import './database/mongodb.database';
 
-NotificationService.fetchAndSentNotifications
+
+const sentNotificationAsync = async () => {
+  await NotificationService.fetchAndSentNotifications();
+}
+
+sentNotificationAsync();
