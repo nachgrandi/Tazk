53cff150ebcf36a7fe24ef4eb86731e79a53b824
NachGrandi <nachgrandi@gmail.com>
2021-11-09 20:09:03 -0300

    ''\

--- src/notification.ts
+++ src/notification.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios
   .post('https://tazk-app.herokuapp.com/notifications')
   .then((res) => {
-    console.log(`statusCode: ${res.status}, ${res.data.msg}`)
+    console.log(`statusCode: ${res.status}, message: ${res.data.msg}`)
   })
   .catch(error => {
     console.error(error)
